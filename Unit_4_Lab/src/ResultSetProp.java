fd9cbb87442cc6c2e85672116ac248fcb27a43ee
Muskan Bhandari <bmuskan007@gmail.com>
2023-04-10 10:43:12 +0545

    added additional result set prop

---
+++ Unit_4_Lab/src/ResultSetProp.java
@@ -0,0 +1,50 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ResultSetProp {
+	Connection con;
+	public ResultSetProp() throws ClassNotFoundException, SQLException {
+		
+		con = new Database().getConnection();
+        Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Student");
+
+        // Print the initial contents of the ResultSet
+
+        System.out.println("Initial contents of ResultSet:");
+        printResultSet(rs);
+
+
+        rs.absolute(1);
+        
+        rs.updateString("Lastname","Doe" );
+        rs.updateRow();
+
+        System.out.println("Contents of updating");
+        printResultSet(rs);
+        
+
+        rs.close();
+        stmt.close();
+        con.close();
+			
+		
+	}
+	private static void printResultSet(ResultSet rs) throws SQLException {
+	    rs.beforeFirst();
+	    while (rs.next()) {
+	        System.out.println(rs.getString("Firstname") + "\t" + rs.getString("Lastname"));
+	    }
+	    System.out.println();
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		new ResultSetProp();
+	}
+
+}
+
